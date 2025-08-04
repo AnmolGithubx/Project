6899cc6eb1a70bcb034cee28268fefdee1b0a85e
Anmol <146084770+AnmolGithubx@users.noreply.github.com>
2025-08-04 17:16:32 +0000

    Started the Hero Section

---
+++ StudySync/src/Landing.jsx
@@ -0,0 +1,14 @@
+import './Landing.css'
+
+function Landing(){
+    return(
+        <>
+        <div className="hero-div">
+            <h1 className="hero-title">Study Smarter, Together</h1>
+            <p className='hero-content'>Join thousands of students in collaborative study rooms. Share knowledge, track progress, and ace your competitive exams with our comprehensive learning platform.</p>
+            <a href='#' className='hero-button'>Start Learning Free</a>
+        </div>
+        </>
+    );
+}
+export default Landing
