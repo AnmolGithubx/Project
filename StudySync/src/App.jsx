675789fb0e770752fb2cf088b5f983e6beb86768
Anmol <146084770+AnmolGithubx@users.noreply.github.com>
2025-08-07 15:04:09 +0000

    .

--- StudySync/src/App.jsx
+++ StudySync/src/App.jsx
@@ -2,8 +2,6 @@ import Landing from "./Landing";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 import Signup from "./Signup";
-import Flashcardpage from "./FlashcardPage";
-import MockTest from "./MockTest";
 
 function App() {
   return (
@@ -12,8 +10,6 @@ function App() {
       <Dashboard />
       <Login />
       <Signup />
-      <Flashcardpage />
-      <MockTest />
     </>
   );
 }
