0dfe04a0bc1f0dac262bae31570670bf0cf9e46a
Anmol <146084770+AnmolGithubx@users.noreply.github.com>
2025-08-07 15:01:36 +0000

    made mocktest page

--- StudySync/src/App.jsx
+++ StudySync/src/App.jsx
@@ -2,6 +2,8 @@ import Landing from "./Landing";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 import Signup from "./Signup";
+import Flashcardpage from "./FlashcardPage";
+import MockTest from "./MockTest";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Dashboard />
       <Login />
       <Signup />
+      <Flashcardpage />
+      <MockTest />
     </>
   );
 }
